b33bb96fe523b38bc4439a86a0f7543c0dd3718e
yuliiadolishnia24-svg <yuliia.dolishnia.24@pnu.edu.ua>
2025-12-14 19:39:59 +0200

    Delete app directory

--- app/accounts/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-accounts_bp = Blueprint('accounts', __name__, template_folder='templates')
-
-
-from . import routes
--- app/products/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-products_bp = Blueprint('products', __name__, template_folder='templates')
-
-@products_bp.route("/list")
-def product_list():
-    products = ["Яблуко", "Банан", "Апельсин"]
-    return render_template("products/list.html", products=products)
